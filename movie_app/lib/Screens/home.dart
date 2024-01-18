9783e0b97d8ab2057294df383f595c04112a2cda
KNURanasinghe <113103000+KNURanasinghe@users.noreply.github.com>
2024-01-19 03:36:49 +0530

    first page done with api

--- movie_app/lib/Screens/home.dart
+++ movie_app/lib/Screens/home.dart
@@ -12,35 +12,84 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   ApiService service = ApiService();
   List<Movie> movies = [];
+  int page = 1;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: FutureBuilder(
-      future: service.getMovies(),
-      builder: (context, snapshot) {
-        if (snapshot.hasData) {
-          movies = snapshot.data!;
-          return GridView.builder(
-            itemCount: movies.length,
-            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 2,
-                mainAxisSpacing: 5,
-                crossAxisSpacing: 5,
-                childAspectRatio: 0.65),
-            itemBuilder: (context, index) {
-              return Column(
-                children: [
-                  Container(
-                    decoration: const BoxDecoration(color: Colors.grey,image: DecorationImage(image: NetworkImage("https://image.tmdb.org/t/p/w500"))),
-                  ),
-                ],
-              );
-            },
-          );
-        }
-        return const Center(child: CircularProgressIndicator());
-      },
+        body: Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            const Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Icon(
+                  Icons.menu,
+                ),
+                Text(
+                  "TMDB movies",
+                  style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+                ),
+                Icon(Icons.favorite)
+              ],
+            ),
+            FutureBuilder(
+              future: service.getMovies(page: page),
+              builder: (context, snapshot) {
+                if (snapshot.hasData) {
+                  movies = [...movies, ...snapshot.data!];
+                  movies = movies.toSet().toList();
+                  return GridView.builder(
+                    shrinkWrap: true,
+                    physics: const NeverScrollableScrollPhysics(),
+                    itemCount: movies.length,
+                    gridDelegate:
+                        const SliverGridDelegateWithFixedCrossAxisCount(
+                            crossAxisCount: 2,
+                            mainAxisSpacing: 5,
+                            crossAxisSpacing: 5,
+                            childAspectRatio: 0.59),
+                    itemBuilder: (context, index) {
+                      return Column(
+                        children: [
+                          Flexible(
+                            child: Container(
+                              decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(10),
+                                  color: Colors.grey,
+                                  image: DecorationImage(
+                                      fit: BoxFit.fitHeight,
+                                      image: NetworkImage(
+                                          "https://image.tmdb.org/t/p/w500${movies[index].posterPath}"))),
+                            ),
+                          ),
+                          Text(
+                            movies[index].title.toString(),
+                            style: const TextStyle(
+                              overflow: TextOverflow.ellipsis,
+                              fontWeight: FontWeight.w700,
+                            ),
+                          )
+                        ],
+                      );
+                    },
+                  );
+                }
+                return const Center(child: CircularProgressIndicator());
+              },
+            ),
+            ElevatedButton(
+                onPressed: () {
+                  setState(() {
+                    page++;
+                  });
+                },
+                child: const Text("Load More"))
+          ],
+        ),
+      ),
     ));
   }
 }
--- movie_app/lib/services/api_sevices.dart
+++ movie_app/lib/services/api_sevices.dart
@@ -7,15 +7,15 @@ class ApiService {
   final aiken = "api_key=06f55ef6b15be934f95b8147bdc4a4bc";
   final popular = "https://api.themoviedb.org/3/movie/popular?";
 //send request
-  Future<List<Movie>> getMovies() async {
-    Response response = await get(Uri.parse("$popular$aiken"));
-   // check responce status code
+  Future<List<Movie>> getMovies({required int page}) async {
+    Response response = await get(Uri.parse("$popular$aiken&page=$page"));
+    // check responce status code
     if (response.statusCode == 200) {
       //extract responce body
       Map<String, dynamic> body = jsonDecode(response.body);
       //extarct result from body
       List<dynamic> data = body['results'];
-     // map to movie modal
+      // map to movie modal
       List<Movie> movies = data.map((movie) => Movie.fromJason(movie)).toList();
       return movies;
     } else {
